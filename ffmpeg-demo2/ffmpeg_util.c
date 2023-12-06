b31c927ec4a9cf411f21f959c78ce36b3c489fa0
刘永昌 <icmilyc@163.com>
2023-12-06 16:22:23 +0800

    能够调用C语言了

---
+++ ffmpeg-demo2/ffmpeg_util.c
@@ -0,0 +1,11 @@
+//
+//  ffmpeg_util.c
+//  ffmpeg-demo2
+//
+//  Created by liuyongchang on 2023/12/6.
+//
+
+#include "ffmpeg_util.h"
+void hello_ffmpeg(void){
+    printf("Hello this form ffmpeg util\n");
+}
---
+++ ffmpeg-demo2/ffmpeg_util.h
@@ -0,0 +1,14 @@
+//
+//  ffmpeg_util.h
+//  ffmpeg-demo2
+//
+//  Created by liuyongchang on 2023/12/6.
+//
+
+#ifndef ffmpeg_util_h
+#define ffmpeg_util_h
+
+#include <stdio.h>
+void hello_ffmpeg(void);
+
+#endif /* ffmpeg_util_h */
